2390119f1ed0519b4153bc58a52ef15d13ea72f5
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-09-30 12:50:11 +0100

    Link homepage to locations. Set up form for user's input

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'home',       to: 'static_pages#contact'
-  get '/locations', to: 'static_pages#locations'
-  get  '/help',     to: 'static_pages#help'
-  get 'contact',    to: 'static_pages#contact'
+  get '/home',       to: 'static_pages#home'
+  #get '/locations', to: 'static_pages#locations'
+  get '/help',     to: 'static_pages#help'
+  get '/contact',    to: 'static_pages#contact'
 
   resources :locations
 
