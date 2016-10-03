e975a07b619b9a1d93e16dd111870bde590f4d63
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-10-03 12:08:47 +0100

    Add methods to Edit and Destroy locations. Display all locations on /locations page

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,16 +9,38 @@ class LocationsController < ApplicationController
   end
 
   def show
-    @location = Location.find(params[:location])
+    @location = Location.find(params[:id])
   end
 
   def create
-    @location = Location.new(locations_params)
+    @location = Location.new(location_params)
     if @location.save
+      flash[:success] = "New location added!"
       redirect_to @location
     else
       render "new"
     end
+  end
+
+  def edit
+    @location = Location.find(params[:id])
+  end
+
+  def update
+    @location = Location.find(params[:id])
+    if @location.update_attributes(location_params)
+      # Handle a successful update
+      flash[:success] = "Location updated!"
+      redirect_to @location
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    Location.find(params[:id]).destroy
+    flash[:success] = "Location deleted"
+    redirect_to locations_url
   end
 
   private
