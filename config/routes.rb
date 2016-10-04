1aff0387e72e2eefd4607bb054ec5649d585afd9
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-10-04 16:37:19 +0100

    Initial API

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,12 @@ Rails.application.routes.draw do
 
   resources :locations
 
+  #api
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+    namespace :v1 do
+      resources :locations, only: [:index, :new, :show, :edit, :destroy]
+    end
+  end
+
+
 end
