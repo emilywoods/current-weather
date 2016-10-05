77646f3f8dc7ba06a4c664683be145334487c950
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-10-05 17:45:32 +0100

    Changes to deserialiser

--- app/serializers/api/v1/deserializer.rb
+++
@@ -1,7 +0,0 @@
-def jsonapi_parse(location)
-  ActiveModelSerializers::Deserialization.jsonapi_parse(location)
-end
-
-def jsonapi_parse!(location)
-  ActiveModelSerializers::Deserialization.jsonapi_parse!(location)
-end
