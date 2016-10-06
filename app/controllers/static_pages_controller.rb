133f00e95d14150257d62b5f464e0a793bb5ea12
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-10-06 16:16:09 +0100

    Implement changes as per pull req

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,14 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
   end
-
-  def help
-  end
-
-  def contact
-  end
-
-  def locations
-  end
-  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/home',       to: 'static_pages#home'
-  #get '/locations', to: 'static_pages#locations'
-  get '/help',     to: 'static_pages#help'
-  get '/contact',    to: 'static_pages#contact'
 
   resources :locations
 
