b865e475b0350e448d55c0951b1930ed3a16dbfd
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-10-06 14:18:19 +0100

    Migration for unique latitude and longitudes. Further routes and layout tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< 331cdac7d96c571c2e87879d7831abdae57d5f37
-ActiveRecord::Schema.define(version: 20161013161011) do
-=======
-ActiveRecord::Schema.define(version: 20161013100145) do
->>>>>>> Change new of DB to WeatherRecording from CurrentWeather. Connect weather to locations db
+ActiveRecord::Schema.define(version: 20161019113443) do
+
+  create_table "delayed_jobs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "priority",                 default: 0, null: false
+    t.integer  "attempts",                 default: 0, null: false
+    t.text     "handler",    limit: 65535,             null: false
+    t.text     "last_error", limit: 65535
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+  end
 
   create_table "locations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
@@ -34,10 +45,7 @@ ActiveRecord::Schema.define(version: 20161013100145) do
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.integer  "location_id"
-<<<<<<< 331cdac7d96c571c2e87879d7831abdae57d5f37
     t.datetime "recorded_at"
-=======
->>>>>>> Change new of DB to WeatherRecording from CurrentWeather. Connect weather to locations db
   end
 
 end
