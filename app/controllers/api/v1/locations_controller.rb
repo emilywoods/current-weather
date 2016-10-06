95956c9419339ccad816dfb1c5aa1779f4aa0f48
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-10-06 10:50:23 +0100

    Update api-controllerfor xml. Changes to Api Deserializer

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -3,15 +3,23 @@ class Api::V1::LocationsController < Api::V1::BaseController
   def show
     location = Location.find(params[:id])
 
-    render(json: Api::V1::LocationSerializer.new(location).to_json)
+    respond_to do |format|
+      format.html {render Api::V1::LocationDeserializer.new(location)}
+      format.json {render(json: Api::V1::LocationSerializer.new(location).to_json)}
+      #why not use format.json { render :json => location}??
+    end
   end
 
-  def new
+  def create
     location = Location.new(location_params)
-    if location.save
-      render json: location, status: 201, location: [:api, location]
-    else
-      render json: { errors: user.error }, status: 422
+    respond_to do |format|
+      if location.save
+        format.json {render json: location, status: 201, location: [:api, location]}
+        format.xml { render xml: location, status: 201}
+      else
+        format.json {render json: location.errors, status: 422}
+        format.xml { render xml: location.errors, status: 422}
+      end
     end
   end
 
@@ -19,29 +27,38 @@ class Api::V1::LocationsController < Api::V1::BaseController
     locations = Location.all
 
     respond_to do |format|
-      format.json { render :json => locations}
-      format.xml { render xml: locations}
+      format.html {render :text => locations.html_content}
+      format.json { render :json => locations }
+      format.xml { render :xml => locations }
     end
   end
 
-  def edit
+  def update
     location = Location.find(params[:id])
 
-    if location.edit(location_params)
-      render json: location, status: 200, location: [:api, location]
-    else
-      render json: { errors: location.errors}, status:422
+    respond_to do |format|
+      if location.edit(location_params)
+        format.json {render json: location, status: 200, location: [:api, location]}
+        format.xml { render xml: location, status: 200}
+      else
+        format.json {render json: location.errors, status:422}
+        format.xml {render xml: location.errors, status:422}
+      end
     end
   end
 
   def destroy
     location = Location.find(params[:id])
 
-    if !location.destroy
-      return api_error(status: 500)
+    respond_to do |format|
+      if location.destroy
+        format.json {render json: location, status: 204}
+        format.xml { render xml: location, status: 204}
+      else
+        format.json {render json: location.errors, status:500}
+        format.xml {render xml: location.errors, status:500}
+      end
     end
-
-    head status: 204
   end
 
   private
@@ -49,6 +66,4 @@ class Api::V1::LocationsController < Api::V1::BaseController
     def location_params
       params.require(:locations).permit(:name, :latitude, :longitude)
     end
-
-
 end
--- app/serializers/api/v1/location_deserializer.rb
+++ app/serializers/api/v1/location_deserializer.rb
@@ -1,5 +1,6 @@
 class Api::V1::Deserializer
   def jsonapi_parse(location)
-    
+    json_string = "#{location[:name]} #{location[:latitude]} #{location[:longitude]}"
+    return JSON.parse(json_string)
   end
 end
