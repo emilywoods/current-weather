d7e309a92cb681a81d1af7fd46c97503119d8225
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-10-07 11:27:57 +0100

    Remove unnecessary contact and help page tests

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -3,8 +3,6 @@ require 'test_helper'
 class ApplicationHelperTest < ActionView::TestCase
   test "full title helper" do
     assert_equal full_title,  "Weather App"
-    assert_equal full_title("Help"),  "Help | Weather App"
-    assert_equal full_title("Contact"),  "Contact | Weather App"
     assert_equal full_title("Locations"),  "Locations | Weather App"
   end
 end
