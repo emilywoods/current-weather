c431e53ec892fcd1eb106a54d66c21d157e344de
Emily Woods <emilywoods@TABLT0131-EmilyWoods.local>
2016-10-12 17:33:24 +0100

    Add relationship to locations and current weather

--- app/models/current_weather.rb
+++ app/models/current_weather.rb
@@ -1,18 +1,20 @@
 require 'json'
 require 'httparty'
 
-class CurrentWeather
+class CurrentWeather < ApplicationRecord
+  belongs_to :location
 
-  attr_accessor:temp_F, :current_summary, :latitude, :longitude
+  attr_accessor :temp_F, :current_summary
 
+  darksky_api_key = 'b94784c3a11a7155afffa4ca04162dec'
 
-  def initialize(latitude, longitude)
+  def initialize
+    @latitude = @location.latitude
+    @longitude = @location.longitude
+  end
 
-    @latitude = latitude
-    @longitude = longitude
-    @darksky_api_key = 'b94784c3a11a7155afffa4ca04162dec'
+  def self.update_weather
 
-    #darksky_api_url = "https://api.darksky.net/forecast/#{ENV["darksky_api_key"]}/#{@latitude},#{@longitude}"
     darksky_api_url = "https://api.darksky.net/forecast/#{@darksky_api_key}/#{@latitude},#{@longitude}"
 
     response = HTTParty.get(darksky_api_url)
@@ -20,7 +22,8 @@ class CurrentWeather
 
     self.temp_F = weather_data['currently']['temperature'] # C = (F-32)/1.8
     self.current_summary = weather_data['currently']['summary']
-
   end
 
 end
+
+#CurrentWeather.create(self.temp_F, self.current_summary)
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,10 @@
 class Location < ApplicationRecord
+  has_many :current_weathers
   before_save { name.downcase! }
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :latitude, presence: true, length: { maximum: 4 }, uniqueness: { case_sensitive: false }
   validates :longitude, presence: true, length: { maximum: 4 }, uniqueness: { case_sensitive: false }
+
 
 end
